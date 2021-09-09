4aae913a749446fa4c4d451de86e27136f0272a6
as1134 <as1134@duke.edu>
2021-09-09 00:09:10 -0400

    firstcommit

--- 029_power_rec/power.c
+++ 029_power_rec/power.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 unsigned power(unsigned x, unsigned y) {
-  if (y < 0) {
+  if (x == 0) {
     return 0;
   }
-  else if (y == 0) {
+  if (y == 0) {
     return 1;
   }
   return x * power(x, y - 1);
