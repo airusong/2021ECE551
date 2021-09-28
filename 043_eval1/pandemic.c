41ba7ad00c26f071e26b17fbe098ef0bb2f3de32
as1134 <as1134@duke.edu>
2021-09-28 03:09:58 -0400

    firstcommit

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -1,11 +1,10 @@
 #include "pandemic.h"
 
-#include <string.h>
-
 #include "ctype.h"
+#include "errno.h"
 #include "inttypes.h"
 #include "stdio.h"
-
+#include "string.h"
 country_t parseLine(char * line) {
   //WRITE ME
   const char * point = line;
@@ -21,34 +20,35 @@ country_t parseLine(char * line) {
   ans.name[0] = '\0';
   ans.population = 0;
   int i = 0;
+  //read contry
   while ((*point) != ',' && i < MAX_NAME_LEN) {
     ans.name[i] = *point;
     i++;
     point++;
-  }
+  }  //put char in the array
   if (i != MAX_NAME_LEN) {
-    ans.name[i] = '\0';
+    ans.name[i] = '\0';  //add a null terminator
   }
   if (i == MAX_NAME_LEN && *point != ',') {
     fprintf(stderr, "Country name larger than 64 bits");
     exit(EXIT_FAILURE);
-  }
+  }  //country name out of bound
   //read digit
   if (*point == ',') {
     point++;
-  }
-  while (*point >= '0' && *point <= '9') {  //transform the population value
-    ans.population = ans.population * 10 + (uint64_t)(*point - '0');
+  }  //pass the comma
+  if (*point == '-') {
+    point++;
+  }  //check the minus sign if it has
+  while (*point >= '0' && *point <= '9') {
+    ans.population = ans.population * 10 +
+                     (uint64_t)(*point - '0');  ////transform the population value
     point++;
   }
-  if (*point != '\n') {
-    fprintf(stderr, "the population is invalid");
+  if (errno == ERANGE) {
+    fprintf(stderr, "The population is out of range\n");
     exit(EXIT_FAILURE);
-  }
-  if (ans.population == 0) {
-    fprintf(stderr, "The population cannot be 0!\n");
-    exit(EXIT_FAILURE);
-  }
+  }  //check if population is out of bound
   return ans;
 }
 
@@ -74,17 +74,20 @@ void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) 
   const unsigned * dataP = data;
   if (data == NULL) {
     exit(EXIT_FAILURE);
-  }
+  }  //check the data pointer
   double * cumP = cum;
   *cumP = (double)(*dataP);
   for (size_t i = 1; i < n_days; i++) {
-    *(cum + i) = *(cum + i - 1) + (double)(*(data + i));
-    if (*(cum + i) > (double)pop) {
+    *(cum + i) = *(cum + i - 1) +
+                 (double)(*(data + i));  //caculate the accumulative cases in n_days
+    if (*(cum + i) >
+        (double)pop) {  //if the accumulative cases is larger than population,exit failure
       exit(EXIT_FAILURE);
     }
   }
   for (size_t j = 0; j < n_days; j++) {
-    *(cum + j) = *(cum + j) * 100000 / pop;
+    *(cum + j) =
+        *(cum + j) * 100000 / pop;  //transform the result to cases per 100000 people
   }
 }
 
