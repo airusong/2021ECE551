32d419b1b0d4d46ffe854d202226e738f1b79194
as1134 <as1134@duke.edu>
2021-09-28 23:04:26 -0400

    firstcommit

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -13,8 +13,52 @@ int stringOrder(const void * vp1, const void * vp2) {
 void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
-
+void operation(FILE * stream) {
+  char ** lines = NULL;
+  char * curr = NULL;
+  size_t sz;
+  size_t i = 0;
+  while (getline(&curr, &sz, stream) >= 0) {
+    lines = realloc(lines, (i + 1) * sizeof(*lines));
+    lines[i] = curr;
+    curr = NULL;
+    i++;
+  }
+  free(curr);
+  sortData(lines, i);
+  for (size_t j = 0; j < i; j++) {
+    printf("%s", lines[j]);
+    free(lines[j]);
+  }
+  free(lines);
+}
 int main(int argc, char ** argv) {
+  if (argc == 1) {
+    FILE * f = stdin;
+    if (f == NULL) {
+      perror("Could not open file");
+      return EXIT_FAILURE;
+    }
+    operation(f);
+    if (fclose(f) != 0) {
+      perror("Failed to close the file");
+      return EXIT_FAILURE;
+    }
+  }
+  if (argc > 1) {
+    for (int i = 0; i < argc; i++) {
+      FILE * f = fopen(argv[i], "r");
+      if (f == NULL) {
+        perror("Could not open file");
+        return EXIT_FAILURE;
+      }
+      operation(f);
+      if (fclose(f) != 0) {
+        perror("Failed to close the file");
+        return EXIT_FAILURE;
+      }
+    }
+  }
   //WRITE YOUR CODE HERE!
 
   return EXIT_SUCCESS;
