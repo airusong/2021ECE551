e5689f7cf973ce9b4d9ed94def20f675e05cadfd
as1134 <as1134@duke.edu>
2021-10-06 14:14:21 -0400

    firstcommit

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,16 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
+  char * suffix = ".counts";
+  int inputLen = strlen(inputName);
+  int suffixLen = strlen(suffix);
+  char * outputFile = malloc((inputLen + suffixLen + 1) * sizeof(*outputFile));
+  strcpy(outputFile, inputName);
+  strcat(outputFile, suffix);
+  return outputFile;
   //WRITE ME
 }
